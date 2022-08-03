63f8a4c6dda60250c9a8b6de098421bb860efec3
M Atheer <55885230+masreplay@users.noreply.github.com>
2022-08-03 11:36:12 +0300

    add random

---
+++ lib/random.dart
@@ -0,0 +1,32 @@
+import 'dart:math';
+
+import 'package:google_maps_flutter/google_maps_flutter.dart';
+import 'package:map_utils/map_utils.dart';
+
+extension on Random {
+  int uniform(int start, int end) {
+    return start + nextInt(end - start);
+  }
+
+  double uniformDouble(double start, double end) {
+    return nextDouble() * (end - start) + start;
+  }
+}
+
+LatLng getRandomPointInPolygon(Polygon polygon) {
+  final bounds = polygon.bounds;
+  while (true) {
+    final point = LatLng(
+      Random().uniformDouble(
+        bounds.northeast.latitude,
+        bounds.southwest.latitude,
+      ),
+      Random().uniformDouble(
+        bounds.northeast.longitude,
+        bounds.southwest.longitude,
+      ),
+    );
+
+    if (polygon.contains(point)) return point;
+  }
+}
